b0e5307dda9cca3ebc9763e10d273682934024ec
Naka Freitag <spamnaka@gmail.com>
2024-09-29 13:18:35 -0400

    Made application slash commands global

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const token = process.env.TOKEN;
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 
 // Register your slash commands
 client.commands = new Collection();
